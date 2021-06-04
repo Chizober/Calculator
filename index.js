c2a0203f08e04857e76063f7633a455aa1ff0fd1
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2021-06-04 21:46:07 +0100

    Update index.js
    Few changes

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+
 const btns = document.querySelectorAll(".btn");
 const screen = document.querySelector(".screen");
 const equalBtn = document.querySelector(".btn-equal");
+const backSpaceBtn = document.querySelector(".btn-backSpace");
 const clearBtn = document.querySelector(".btn-clear");
-for (let i =0; i< btns.length;i++){
- btns[i].addEventListener("click",function(){
-     let number =btns[i].getAttribute("data-num");
-     screen.value +=number
- })
-}
-    equalBtn.addEventListener("click", function(){
-        
- let value = eval(screen.value);
- screen.value = value;
+const ceBtn = document.querySelector(".btn-ce");
+for (let i =0; i< btns.length;i++){ btns[i].addEventListener("click",function(){ let
+    number=btns[i].getAttribute("data-num"); screen.value +=number }) } equalBtn.addEventListener("click", function(){
+    let value=eval(screen.value); screen.value=value; })
+    
+     ceBtn.addEventListener("click", ()=>{
+    location.reload();
     })
-clearBtn.addEventListener("click", function () {
-            screen.value = "";
+
+    clearBtn.addEventListener("click", ()=>{
+    screen.value = "";
     })
-  
+    
+    backSpaceBtn.addEventListener("click", ()=>{
+
+    screen.value = screen.value.slice(0,screen.value.length-1);
+    screen.innerHTML = screen.value;
+    })
+    
 
